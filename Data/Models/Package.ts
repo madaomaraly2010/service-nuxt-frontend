55c94c0fb030bb9d8895fb3a2bedc672ee7c73bc
madaomaraly2010@yahoo.com <madaomaraly2010@yahoo.com>
2025-02-16 08:35:40 +0200

    commit

--- Data/Models/Package.ts
+++ Data/Models/Package.ts
@@ -1,4 +1,5 @@
 import type { PackageItem, Provider } from ".";
+import type { IPackageAttributes } from "../Models-Row-Attributes";
 
 
 export class Package  {
@@ -10,35 +11,30 @@ export class Package  {
   updated_at?: Date;
 
   // Package hasMany PackageItem via package_id
-  packageItems!: PackageItem[];
-  
-  
-  providers!: Provider[];
-  
+  packageItemList!: PackageItem[];
+  providerList!: Provider[];
 
-
-
-  public  toJson():string {
-    return JSON.stringify({
+  public  toDbRow():Package {
+    return {
       id:this.id,
       arb_name:this.arb_name,
       eng_name:this.eng_name,
-      flag_url : this.flag_url,
-      iso_code : this.iso_code,
       created_at : this.created_at,
-      updated_at : this.updated_at,
-    })
+      updated_at : this.updated_at
+    }
+  
 }
 
-public static fromJson(json:any):Package {
+public static fromDbRow(dbRow:IPackageAttributes):Package {
 let row:Package = new Package();
- row.id = json.id
- row.arb_name = json.arb_name
- row.eng_name = json.eng_name
- row.is_active = json.is_active
- row.created_at = json.created_at
- row.updated_at = json.updated_at
- row.providers = 
+ row.id = dbRow.id
+ row.arb_name = dbRow.arb_name
+ row.eng_name = dbRow.eng_name
+ row.is_active = dbRow.is_active
+ row.created_at = dbRow.created_at
+ row.updated_at = dbRow.updated_at
+ if(dbRow.package_items)
+     row.packageItemList = dbRow.package_items.map(it=>PackageItem.fromDbRow(it))
 return row;
 }
 
